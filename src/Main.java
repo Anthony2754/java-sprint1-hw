5b84e9b55ba5861042532831a7563d5d35ab8fdf
Anton Bobrov <anthony020@yandex.ru>
2022-11-02 21:35:19 +0300

    6 версия

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Пришло время практики!");
+        int target = 10000;
+        Scanner scanner = new Scanner(System.in);
+        StepTracker stepTracker = new StepTracker();
+        System.out.println("Ваша ежедневная цель: " + target + " шагов.");
+        printMenu();
+        int userInput = scanner.nextInt();
+        while (userInput != 0) {
+
+            if (userInput == 1) {
+                System.out.println("За какой месяц вы хотите ввести количество шагов: 1 - Январь, 2 - Февраль, 3 - Март, 4 - Апрель, 5 - Май, 6 - Июнь, 7 - Июль, 8 - Август, 9 - Сентябрь, 10 - Октябрь, 11 - Ноябрь, 12 - Декабрь");
+                int month = scanner.nextInt();
+                System.out.println("За какой день вы хотите ввести количество шагов: от 1 до 30.");
+                int day = scanner.nextInt();
+                System.out.println("Введите количество ваших шагов:");
+                int steps = scanner.nextInt();
+                stepTracker.saveStepTracker(month, day, steps);
+
+            } else if (userInput == 2) {
+                System.out.println("За какой месяц вы хотите посмотреть статистику: 1 - Январь, 2 - Февраль, 3 - Март, 4 - Апрель, 5 - Май, 6 - Июнь, 7 - Июль, 8 - Август, 9 - Сентябрь, 10 - Октябрь, 11 - Ноябрь, 12 - Декабрь");
+                int month = scanner.nextInt();
+                stepTracker.Statistic(month);
+
+            } else if (userInput == 3) {
+                System.out.println("Введите новую цель по количеству шагов в день:");
+                int newTargetSteps = scanner.nextInt();
+                stepTracker.changePurposeOfDailySteps(newTargetSteps);
+
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
+
+            }
+            printMenu();
+            userInput = scanner.nextInt();
+        }
+        System.out.println("Программа завершена");
+    }
+
+    public static void printMenu() {
+        System.out.println("Что вы хотите сделать?");
+        System.out.println("1 - Ввести количество шагов за определённый день");
+        System.out.println("2 - Напечатать статистику за определённый месяц");
+        System.out.println("3 - Изменить цель по количеству шагов в день");
+        System.out.println("0 - Выйти из приложения");
     }
 }
---
+++ src/StepTracker.java
@@ -0,0 +1,96 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+public class StepTracker {
+    HashMap<Integer, ArrayList<Integer>> stepsInMonth;
+    ArrayList<Integer> year;
+    int targetSteps = 10000;
+    int stepInDay;
+    StepTracker() {
+        stepsInMonth = new HashMap<>();
+        stepInDay = 0;
+        stepsInMonth.put(1, initArrayList());
+        stepsInMonth.put(2, initArrayList());
+        stepsInMonth.put(3, initArrayList());
+        stepsInMonth.put(4, initArrayList());
+        stepsInMonth.put(5, initArrayList());
+        stepsInMonth.put(6, initArrayList());
+        stepsInMonth.put(7, initArrayList());
+        stepsInMonth.put(8, initArrayList());
+        stepsInMonth.put(9, initArrayList());
+        stepsInMonth.put(10, initArrayList());
+        stepsInMonth.put(11, initArrayList());
+        stepsInMonth.put(12, initArrayList());
+    }
+    public ArrayList<Integer> initArrayList() {
+        year = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            year.add(stepInDay);
+        }
+        return year;
+    }
+
+    public void saveStepTracker(int month, int day, int steps) {
+        if (month < 1 | month > 12) {
+            System.out.println("Введен несуществующий месяц");
+            return;
+        } else if (day < 1 | day > 30) {
+                System.out.println("Введен несуществующий день");
+                return;
+            } else if (steps < 0) {
+                    System.out.println("В количестве шагов недопустимо использование отрицательных чисел");
+                    return;
+                }
+
+
+            if (stepsInMonth.containsKey(month)) {
+                stepsInMonth.get(month).set((day - 1), steps);
+                System.out.println("Значение сохранено!");
+            }
+
+    }
+
+    public void Statistic(int month) {
+        int sumSteps = 0;
+        int maxSteps = 0;
+        int maxStepsInDay = 0;
+        int bestSeries = 0;
+        int thisSeries = 0;
+        if (stepsInMonth.containsKey(month)) {
+            for (int i = 0; i < year.size(); i++) {
+                int steps = stepsInMonth.get(month).get(i);
+                if (i != 29) {
+                    System.out.print((i + 1) + " день: " + steps + ", ");
+                } else {
+                    System.out.println((i + 1) + " день: " + steps);
+                }
+                sumSteps += steps;
+                if (steps > maxSteps) {
+                    maxSteps = steps;
+                    maxStepsInDay = i + 1;
+                }
+                if (steps > targetSteps) {
+                    thisSeries++;
+                } else {
+                    bestSeries = Math.max(bestSeries, thisSeries);
+                    thisSeries = 0;
+                }
+            }
+            int averageSteps = sumSteps / year.size();
+            System.out.println("Всего шагов за месяц: " + sumSteps);
+            System.out.println("Максимальное пройденное количество шагов - " + maxSteps + " было пройдено в " + maxStepsInDay + " день.");
+            System.out.println("Среднее количество шагов в день: " + averageSteps);
+            Converter.convert(sumSteps);
+            System.out.println("Лучшая серия: " + bestSeries + " дней");
+        } else {
+            System.out.println("Введен несуществующий месяц");
+        }
+    }
+    void changePurposeOfDailySteps(int newTargetSteps) {
+        if (newTargetSteps < 0) {
+            System.out.println("В количестве шагов недопустимо использование отрицательных чисел");
+        } else {
+            targetSteps = newTargetSteps;
+            System.out.println("Новая цель по количеству шагов: " + targetSteps);
+        }
+    }
+}
